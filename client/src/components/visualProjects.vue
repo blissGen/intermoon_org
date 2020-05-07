bd0b3b1342f076a1f01d44685545a3427ae3624e
blissGenSTLT <bliss.gen.93@gmail.com>
2020-05-07 16:57:59 -0700

    added visual page

---
+++ client/src/components/visualProjects.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="visuals">
+    <img :style"resizeImage" :src"visual.image" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "visuals",
+  data: function() {
+	return {
+
+	}
+  }
+}
+</script>
