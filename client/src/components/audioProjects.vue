74db1d9ad4e25976294b03649754847decbd64d3
blissGenSTLT <bliss.gen.93@gmail.com>
2020-05-06 23:48:30 -0700

    adding p5 instance to landing page

--- client/src/components/audioProjects.vue
+++ client/src/components/audioProjects.vue
@@ -3,13 +3,13 @@
     <ul
       :style="audioDisplay"
       v-for="audio in audios.slice().reverse()"
-      :key="audio.artistName"
+      :key="audio.projectTitle"
     >
       <a target="_blank" rel="noopener noreferrer" :href="audio.link">
         <img
           :style="resizeImage"
           :src="audio.image"
-          :title="audio.artistName + ' - ' + audio.projectName"
+          :title="audio.artistName + ' - ' + audio.projectTitle"
         />
       </a>
     </ul>
@@ -38,105 +38,105 @@ export default {
       audios: [
         {
           artistName: "Audiopane",
-          projectName: "Imaginaction",
+          projectTitle: "Imaginaction",
           image: require("@/assets/audio_images/Audiopane-Imaginaction.jpg"),
           link: "https://audiopane.bandcamp.com/album/imaginaction"
         },
         {
           artistName: "5X5",
-          projectName: "wwall",
+          projectTitle: "wwall",
           image: require("@/assets/audio_images/5X5_wwall.jpg"),
           link: "https://soundcloud.com/5x5_noise/sets/wwall"
         },
         {
           artistName: "Fugue State",
-          projectName: "dress to kill",
+          projectTitle: "dress to kill",
           image: require("@/assets/audio_images/Fugue-State_dress-to-kill.jpg"),
           link: "https://soundcloud.com/the-rhinos/dress-to-kill"
         },
         {
           artistName: "Crane Song",
-          projectName: "Sounds of Sleep",
+          projectTitle: "Sounds of Sleep",
           image: require("@/assets/audio_images/Crane-Song_Sounds-of-Sleep.jpg"),
           link:
             "https://open.spotify.com/artist/2FmT7TQ2nAMhahAB9CkXYr?si=k0IIla05Scixm6oJM-nMSA"
         },
         {
           artistName: "5X5",
-          projectName: "Storyteller",
+          projectTitle: "Storyteller",
           image: require("@/assets/audio_images/5X5_Storyteller.jpg"),
           link: "https://soundcloud.com/5x5_noise/sets/storyteller"
         },
         {
           artistName: "Fugue State",
-          projectName: "Going Home",
+          projectTitle: "Going Home",
           image: require("@/assets/audio_images/Fugue-State_Going-Home.jpg"),
           link: "https://soundcloud.com/the-rhinos/going-home"
         },
         {
           artistName: "5X5",
-          projectName: "condenser",
+          projectTitle: "condenser",
           image: require("@/assets/audio_images/5X5_condenser.jpg"),
           link: "https://www.youtube.com/watch?v=Htbb33UdhS8&t=13s"
         },
         {
           artistName: "Audiopane",
-          projectName: "Dissimulate",
+          projectTitle: "Dissimulate",
           image: require("@/assets/audio_images/Audiopane_Dissimulate.jpg"),
           link: "https://audiopane.bandcamp.com/album/dissimulate"
         },
         {
           artistName: "5X5",
-          projectName: "orange juice tree",
+          projectTitle: "orange juice tree",
           image: require("@/assets/audio_images/5X5_orange-juice-tree.jpg"),
           link: "https://soundcloud.com/5x5_noise/orange-juice-tree"
         },
         {
           artistName: "Audiopane",
-          projectName: "Step Inside",
+          projectTitle: "Step Inside",
           image: require("@/assets/audio_images/Audiopane_Step-Inside.jpg"),
           link: "https://audiopane.bandcamp.com/album/step-inside"
         },
         {
           artistName: "Keith Blond",
-          projectName: "Safe Expectations",
+          projectTitle: "Safe Expectations",
           image: require("@/assets/audio_images/Keith-Blond_Safe-Expectations.jpg"),
           link: "https://soundcloud.com/keithblond/safe-expectations"
         },
         {
           artistName: "Audiopane",
-          projectName: "Enamored",
+          projectTitle: "Enamored",
           image: require("@/assets/audio_images/Audiopane_Enamored.jpg"),
           link: "https://audiopane.bandcamp.com/album/enamored"
         },
         {
           artistName: "Audiopane",
-          projectName: "Schizo Pel",
+          projectTitle: "Schizo Pel",
           image: require("@/assets/audio_images/Audiopane_Schizo-Pel.jpg"),
           link: "https://audiopane.bandcamp.com/album/schizo-pel"
         },
         {
           artistName: "5X5",
-          projectName: "greenery",
+          projectTitle: "greenery",
           image: require("@/assets/audio_images/5X5_greenery.jpg"),
           link: "https://soundcloud.com/5x5_noise/sets/greenery"
         },
         {
           artistName: "Keith Blond",
-          projectName: "In The Moment Where Everything Makes Sense",
+          projectTitle: "In The Moment Where Everything Makes Sense",
           image: require("@/assets/audio_images/Keith-Blond_In-The-Moment-Where-Everything-Makes-Sense.jpg"),
           link:
             "https://open.spotify.com/album/3ZrFtZw6ReAjF8TNUwTnEz?si=ocR-we0GRRaJeIfY5en_5A"
         },
         {
           artistName: "Keith Blond",
-          projectName: "SIMYH",
+          projectTitle: "SIMYH",
           image: require("@/assets/audio_images/Keith-Blond_SIMYH.jpg"),
           link: "https://www.youtube.com/watch?v=j-UHad26Hi8"
         },
         {
           artistName: "5X5",
-          projectName: "ultra",
+          projectTitle: "ultra",
           image: require("@/assets/audio_images/5X5_ultra.jpg"),
           link: "https://soundcloud.com/5x5_noise/ultra"
         }
