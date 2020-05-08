233f339a125e106b9d4c6ca4dcff7532924615aa
blissGenSTLT <bliss.gen.93@gmail.com>
2020-05-07 17:26:11 -0700

    added visual page

--- client/src/components/visualProjects.vue
+++ client/src/components/visualProjects.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="visuals">
-    <img :style"resizeImage" :src"visual.image" />
+    <ul
+      :style="visualDisplay"
+      v-for="visual in visuals.slice().reverse()"
+      :key="visual.projectTitle"
+    >
+      <img
+        :style="resizeImage"
+        :src="visual.image"
+        :title="visual.projectTitle"
+      />
+    </ul>
   </div>
 </template>
 
@@ -8,17 +18,23 @@
 export default {
   name: "visuals",
   data: function() {
-	return {
-	  resizeImage: {
-		width: "300px"
-	  },
-	  visuals: [
-		{
-		  projectTitle: "ascii cat",
-		  image: require("@/assets/visual/gif/a.gif")
-		}
-	  ]
-	}
+    return {
+      resizeImage: {
+        width: "600px"
+      },
+      visualDisplay: {
+        display: "inline-block",
+        verticalAlign: "middle",
+        margin: "0px",
+        padding: "20px"
+      },
+      visuals: [
+        {
+          projectTitle: "ascii cat",
+          image: require("@/assets/visual/gif/a.gif")
+        }
+      ]
+    };
   }
-}
+};
 </script>
---
+++ client/src/views/Visual.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="visual">
+    <visualProjects />
+  </div>
+</template>
+
+<script>
+import visualProjects from "@/components/visualProjects";
+export default {
+  name: "Visual",
+  components: {
+    visualProjects
+  }
+};
+</script>
