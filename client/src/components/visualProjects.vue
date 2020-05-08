be3a2ac3fdc01b00379aa5ac32443ab2aeb7e2df
blissGenSTLT <bliss.gen.93@gmail.com>
2020-05-07 17:03:21 -0700

    added visual page

--- client/src/components/visualProjects.vue
+++ client/src/components/visualProjects.vue
@@ -9,7 +9,15 @@ export default {
   name: "visuals",
   data: function() {
 	return {
-
+	  resizeImage: {
+		width: "300px"
+	  },
+	  visuals: [
+		{
+		  projectTitle: "ascii cat",
+		  image: require("@/assets/visual/gif/a.gif")
+		}
+	  ]
 	}
   }
 }
