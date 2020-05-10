8bda2aa064f0a7022dfc65337657e32dd724e2af
blissGenSTLT <bliss.gen.93@gmail.com>
2020-05-10 14:26:22 -0700

    changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ app.use('/api/submissions', submissions);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/public/'));
 
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));	
+  app.get('*', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');	
+  });	
 }
 
 const port = process.env.PORT || 1111;
