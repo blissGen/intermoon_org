bd19d8660f0c58517bb0e1d2d6a2418f6c89b742
blissGenSTLT <bliss.gen.93@gmail.com>
2020-05-09 21:43:09 -0700

    preparing for deployment

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(cors());
 
 const submissions = require('./routes/api/submissions');
